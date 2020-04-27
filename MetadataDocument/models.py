f69637b270b1483a7b30f83d22e285281dac3c94
ALH Software <alh@europe.com>
2020-04-27 03:03:46 +0300

    Migrations

--- MetadataDocument/models.py
+++ MetadataDocument/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 
 class Metadata(models.Model):
-    id = models.BigIntegerField(primary_key=True)
+    id = models.AutoField(auto_created = True, primary_key = True,  serialize = False,)
     metadata_name = models.CharField(max_length=250, unique=True)
     metadata_string = models.TextField()
 
 class Document(models.Model):
-    id = models.BigIntegerField(primary_key=True)
+    id = models.AutoField(auto_created = True, primary_key = True,  serialize = False,)
     document_name = models.CharField(max_length=250, unique=True)
     document_file = models.FileField(max_length=100, blank=False, null=False)
